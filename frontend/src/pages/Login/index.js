b86141613e9b2f47abbf4e76cf6789891c7435e0
Enzo <enzoap2009@hotmail.com>
2020-04-13 19:59:12 -0300

    Criação do frontend e página de login.

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,28 @@
+import React from 'react' 
+import { Link } from 'react-router-dom'
+import {FiLogIn} from 'react-icons/fi'
+
+import './styles.css'
+
+export default function Login(){
+    return (
+        
+        <div className="logon-container">
+            <section className="form">
+            <form>
+                <h1>Faça seu login</h1>
+                
+                <input placeholder="Usuário"/>
+                <input placeholder="Senha"/>
+                <button className="button" type="submit">Entrar</button>
+
+                <Link to="/register ">
+                    <FiLogIn size={16} color="#56bLinked" ></FiLogIn>
+                    Não tenho cadastro
+                </Link>
+            </form>
+            </section>
+
+        </div>
+    )
+}
