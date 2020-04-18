6b35d622bb1ea721a57ca4444d03b684ec540beb
Enzo <enzoap2009@hotmail.com>
2020-04-17 22:33:42 -0300

    Configurado o cors.

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,6 +16,7 @@ mongoose.connect(process.env.DB_URI, {
 })
 mongoose.set('useCreateIndex', true);
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(express.static(path.join(__dirname,'../public')))
