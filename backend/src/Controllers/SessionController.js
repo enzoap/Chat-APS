2f2da8dcd158e065b6ca066a254e7272049b3480
Enzo <enzoap2009@hotmail.com>
2020-04-18 18:05:07 -0300

    Adicionado retorno se admin.

--- backend/src/Controllers/SessionController.js
+++ backend/src/Controllers/SessionController.js
@@ -9,9 +9,17 @@ module.exports = {
             const user = await UserSchema.findOne({name})
             const comparePassword = await bcrypt.compare(password, user.password)
             
-            if(comparePassword && !user.admin){
-                return res.redirect('/chat')
+            if(comparePassword){
+                if(user.admin){
+                    return res.json({admin: true})
+                }else {
+                    return res.json({admin: false})
+                }
             }
+
+            // if(comparePassword && !user.admin){
+            //     return res.redirect('/chat')
+            // }
             return res.json({Login: "Usuário ou senha incorreto"})
         
     }
