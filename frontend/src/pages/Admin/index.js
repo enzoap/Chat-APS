7d5b3e273b059df5cdce482cf49104f532417e6c
Jhefferson Estevanato <j.hefferson@hotmail.com>
2020-05-09 15:40:33 -0300

    finalização do frontend

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,17 +1,17 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import {FiLogIn} from 'react-icons/fi'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi'
 import api from '../../services/api'
 import './styles.css'
 
-export default function Admin(){
+export default function Admin() {
     const [show, setShow] = useState(false)
     const [badWord, setBadWord] = useState('')
 
     const admin = localStorage.getItem('admin')
     const name = localStorage.getItem('name')
 
-    async function deleteMessages(e){
+    async function deleteMessages(e) {
         e.preventDefault()
 
         try {
@@ -26,7 +26,7 @@ export default function Admin(){
         }
     }
 
-    async function handleBadWord(e){
+    async function handleBadWord(e) {
         e.preventDefault()
 
         const data = {
@@ -46,20 +46,20 @@ export default function Admin(){
             <div className="button-group">
                 <button className="button" type='submit' onClick={deleteMessages}>Apagar mensagens do banco</button>
                 <button className="button" onClick={() => setShow(true)}>Inserir mensagem inadequada</button>
-                {show ? 
-                <form className="hiden" onSubmit={handleBadWord}>
-                    <input 
-                    type="text" 
-                    placeholder="Digite uma palavra"
-                    value={badWord}
-                    onChange={e => setBadWord(e.target.value)}
-                    />
-                    <button className="button" >Enviar</button>
-                </form> : null
+                {show ?
+                    <form className="hiden" onSubmit={handleBadWord}>
+                        <input
+                            type="text"
+                            placeholder="Digite uma palavra"
+                            value={badWord}
+                            onChange={e => setBadWord(e.target.value)}
+                        />
+                        <button className="button" >Enviar</button>
+                    </form> : null
                 }
-               
+
                 <Link className="back-link" to="/chat">
-                <FiLogIn size={16} color="#56bLinked" ></FiLogIn>
+                    <FiLogIn size={16} color="#56bLinked" ></FiLogIn>
                     Acessar o chat.
                 </Link>
             </div>
--- frontend/src/pages/Chat/index.js
+++ frontend/src/pages/Chat/index.js
@@ -1,13 +1,13 @@
-import React,{useState,useEffect} from 'react'
-import {Link,useHistory} from 'react-router-dom'
-import {FiPower} from 'react-icons/fi'
+import React, { useState, useEffect } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { FiPower } from 'react-icons/fi'
 import io from 'socket.io-client'
 import './styles.css'
 import { render } from '@testing-library/react'
 
 export default function Chat() {
     const socket = io('https://chat-unip.herokuapp.com/')
-    const [message,setMessage] = useState('')
+    const [message, setMessage] = useState('')
     const name = localStorage.getItem('name')
     let messageObject
     const history = useHistory()
@@ -16,68 +16,69 @@ export default function Chat() {
         author: 'Servidor'
     }
 
-    function handleMessage(e){
+    function handleMessage(e) {
         e.preventDefault()
 
-        if(message.length > 0){
-            messageObject ={
+        if (message.length > 0) {
+            messageObject = {
                 author: name,
                 message
             }
-            socket.emit('sendMessage',messageObject)
+            socket.emit('sendMessage', messageObject)
             setMessage('')
-            
+
         }
     }
-    function renderMessage(message){
+    function renderMessage(message) {
         let divMessage = document.createElement('div')
         divMessage.className = "message"
-        divMessage.innerHTML = "<strong>" +message.author+"</strong>: " + message.message
+        divMessage.innerHTML = "<strong>" + message.author + "</strong>: " + message.message
         document.getElementsByClassName('messages')[0].appendChild(divMessage)
     }
 
     useEffect(() => {
-        socket.on('receivedMessage', function(message){
+        socket.on('receivedMessage', function (message) {
             renderMessage(message)
         })
-    },[])
-    
+    }, [])
 
-    socket.on('blockMessage', function (blockedMessage){
-        if(blockedMessage){
+
+    socket.on('blockMessage', function (blockedMessage) {
+        if (blockedMessage) {
             renderMessage(blockmessage)
             console.log(blockedMessage)
         }
     })
 
-    function logout(){
+    function logout() {
         localStorage.clear()
         alert('Você será desconectado em breve.')
         history.push('/')
     }
     return (
         <>
-        <form id="chat" onSubmit={handleMessage}>
-            <input readOnly={true} id="username" name="username" value={name}/> 
-            <div className="messages"></div>  
-            <input 
-            type="text" 
-            id="message" 
-            name="message" 
-            placeholder="Digite sua mensagem"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-            />
-            <button className="button" type="submit">Enviar</button>
-            <button id="logout" type="button" onClick={logout}>
-                <FiPower size={18} color="E02141"></FiPower>
-            </button>
-            {/* <Link className="back-link" to="/">
+            <form id="chat" onSubmit={handleMessage}>
+                <input readOnly={true} id="username" name="username" value={name} />
+                <div className="messages"></div>
+                <input
+                    className="mes"
+                    type="text"
+                    id="message"
+                    name="message"
+                    placeholder="Digite sua mensagem"
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
+                />
+                <button className="button-e" type="submit">Enviar</button>
+                <button id="logout" type="button" onClick={logout}>
+                    <FiPower size={18} color="E02141"></FiPower>
+                </button>
+                {/* <Link className="back-link" to="/">
                 <FiLogOut size={16} color="56bLinked"> </FiLogOut>
                 Sair
             </Link> */}
 
-        </form>
+            </form>
         </>
     )
 }
